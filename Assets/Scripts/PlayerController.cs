98ab69de304923170532613d9726d3b9f5c03887
ericciria <ericciria_6624@hotmail.com>
2022-05-19 11:40:17 +0200

    bales fix espero

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -82,6 +82,7 @@ public class PlayerController : MonoBehaviour
 
     private void handleMovement()
     {
+        Debug.LogWarning(anim);
         if (moveInput.y == 0 && moveInput.x == 0)
         {
             anim.SetBool("walk", false);
